57390810fc5b5aea64b1db927e33ac943f6af732
zengyh <1181159889@qq.com>
2018-09-03 12:16:56 +0800

    1）整合Log44j2日志打印工具
    2）实现多个环境（开发dev、生产prod、测试test）切换打包配置文件
    
    3）整合Hibernate持久化工具

--- src/main/java/codex/terry/codex/terry/entity/BaseEntity.java
+++ src/main/java/codex/terry/codex/terry/entity/BaseEntity.java
@@ -9,7 +9,11 @@ import java.io.Serializable;
 /**
  * 编写人: yh.zeng
  * 编写时间: 2018-8-28
- * 文件描述:
+ * 文件描述: 实体类的父类模板，子类必须实现 @Entity或@Table注解
+ *
+ * @MappedSuperclass的用法
+ *    注解的类将不是一个完整的实体类，他将不会映射到数据库表，
+ *    但是他的属性都将映射到其子类的数据库字段中，不能再标注@Entity或@Table注解
  */
 @MappedSuperclass
 public class BaseEntity implements Serializable
